19da389672a39967d2624faebec0ee88d04d5c82
Klee Collage <klee.collage@gmail.com>
2024-11-18 17:13:14 -0600

    cors disabled

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import 'dotenv/config';
 import './database/connectDb.js';
 import authRouter from './routes/auth.route.js';
 import linkRouter from './routes/link.route.js';
-import redirectRouter from "./routes/redirect.route.js";
-
+import redirectRoute from './routes/redirect.route.js'
 const app = express();
 const whiteList = [ process.env.ORIGIN1, process.env.ORIGIN2, process.env.ORIGIN3, process.env.ORIGIN4]
 
@@ -14,21 +13,22 @@ const whiteList = [ process.env.ORIGIN1, process.env.ORIGIN2, process.env.ORIGIN
 //   origin: [process.env.ORIGIN1]
 // }))
 //** CORS **//
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || whiteList.includes(origin)) {
-      return callback(null, origin);
-    }
-    return callback("CORS error. Origin: " + origin + " not authorized")
-  }
-}))
+app.use(cors());
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin || whiteList.includes(origin)) {
+//       return callback(null, origin);
+//     }
+//     return callback("CORS error. Origin: " + origin + " not authorized")
+//   }
+// }))
 
 //* MIDLEWARES *// 
 app.use(express.json());
 app.use(cookieParser());
 // app.use(express.static('public')); // only 4 login/token testing
 //* ROUTES *//
-// app.use('/', redirectRouter); // backend redirection examples
+app.use('/', redirectRoute); // backend redirection examples
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/links", linkRouter);
 //* SERVER *//
