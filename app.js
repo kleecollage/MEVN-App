a7d9476033100a98f35efab08a8432e554f5012c
Klee Collage <klee.collage@gmail.com>
2024-11-26 20:48:07 -0600

    type modules enabled

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // MIDDLEWARES //
 app.use(morgan('tiny'))
